11cad6b2cc4d3940cd7b121fb015dcc52a9260c1
gmann-13 <149419885+gmann-13@users.noreply.github.com>
2024-04-23 13:49:55 -0400

    Prepared comms for lego passing

--- messagingclient.py
+++ messagingclient.py
@@ -1,17 +1,25 @@
 import zmq
+import time
 
-context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to lego passing server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+
 
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
+def SendGrabMessage(request):
+    context = zmq.Context()
+    print("Connecting to lego passing server…")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+    
     print("Sending request %s …" % request)
     socket.send(b"Reciever Grabbed")
 
     #  Get the reply.
     message = socket.recv()
+    time.sleep(1)
     print("Received reply %s [ %s ]" % (request, message))
+    if message == b"Passer Releasing":
+        return True
+    else:
+        return SendGrabMessage(request=request+1)
