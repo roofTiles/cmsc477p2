509b9ffc139b60f4de9e8079c7172545ae103430
Florian Grader-Beck <fgraderb@terpmail.umd.edu>
2024-04-25 11:31:11 -0400

    finalized blue line orientation code

--- image_filtering.py
+++ image_filtering.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 import math as mth
 from matplotlib import pyplot as plt
@@ -9,11 +10,13 @@ from robomaster import camera
 if __name__ == '__main__':
     ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="ap")
-    ep_chassis = ep_robot.chassis
     ep_camera = ep_robot.camera
-    ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+    ep_chassis = ep_robot.chassis
+    ep_camera.start_video_stream(display=True, resolution=camera.STREAM_360P)
+    
+    Oriented = False
 
-    while True:
+    while not Oriented:
         try:
             # Input camera feed
             image = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
@@ -53,47 +56,74 @@ if __name__ == '__main__':
                     thet = thet_vals / (i + 1)
 
                     # yaw angle
-                    yaw = mth.pi / 2 - thet
-
+                    yaw = (mth.pi / 2) - thet 
+                    print("yaw", np.rad2deg(yaw))
+                    
                     y = []
                     x = []
 
-                    # determine equation of mean line to plot
-                    if thet < mth.pi / 2:
-                        x0 = r / mth.cos(thet)
-                        y0 = r / mth.cos(mth.pi / 2 - thet)
-                        m = -y0 / x0
+                # determine equation of mean line to plot
+                if thet < mth.pi / 2:
+                    x0 = r / mth.cos(thet)
+                    y0 = r / mth.cos(mth.pi / 2 - thet)
+                    m = -y0 / x0
 
-                    elif thet > mth.pi / 2:
-                        x0 = -r / mth.cos(mth.pi - thet)
-                        y0 = r / mth.cos(thet - mth.pi / 2)
-                        m = -y0 / x0
+                elif thet > mth.pi / 2:
+                    x0 = -r / mth.cos(mth.pi - thet)
+                    y0 = r / mth.cos(thet - mth.pi / 2)
+                    m = -y0 / x0
 
-                    for j in range(0, 641):
-                        if m * j + y0 < 360:
-                            x.append(j)
-                            y.append(m * j + y0)
+                for j in range(0, 641):
+                    if m * j + y0 < 360:
+                        x.append(j)
+                        y.append(m * j + y0)
 
-                    # orient robot
-                    ep_chassis.speed(x=0, y=0, z=np.rad2deg(yaw))
+                # draw a line on the image
+                # domain = [x[0], x[-1]]
+                # rng = [y[0], y[-1]]
+                # plt.plot(domain, rng, color="red", linewidth=1)
+                start_pt = (round(x[0]), round(y[0]))
+                end_pt = (round(x[-1]), round(y[-1]))
+                print("x0 = " + str(x[0]))
+                print("y0 = " + str(y[0]))
+                print("x1 = " + str(x[-1]))
+                print("y1 = " + str(y[-1]))
+                
+                cv2.line(image, start_pt, end_pt, (255, 0, 0), thickness = 2)
 
-                    # insert proportional controller here to move robot forward
+                # orient robot
+                ep_chassis.move(x=0, y=0, z=np.rad2deg(yaw), z_speed=0.3*np.rad2deg(yaw)).wait_for_completed()
+                # ep_chassis.drive_speed(x=0, y=0, z=-np.rad2deg(yaw), timeout=5)
+                time.sleep(1)
 
-                    # draw a line on the image
-                    domain = [x[0], x[-1]]
-                    rng = [y[0], y[-1]]
-                    plt.plot(domain, rng, color="red", linewidth=1)
+                if np.rad2deg(yaw) > -3 and np.rad2deg(yaw) < 3:
+                    Oriented = True
+                    # move robot
+                    y_min = 110  # pixels 124
+                    y_end = 270  # pixels 285
+                    ws_inside_dist = 1.15  # [m]
+                    scale = ws_inside_dist/(y_end - y_min)
+                    pixel_dist = round((y[0] + y[-1])/2)
+                    x_vel = abs(pixel_dist-y_end)*scale
+                    print("pixel distance:", pixel_dist)
+                    print("actual distance", x_vel)
+                    # ep_chassis.drive_speed(x=x_vel, y=0, z=0, timeout=5)
+                    # time.sleep(3)
+                    ep_chassis.move(x=x_vel, y=0, z=0, z_speed=0).wait_for_completed()
+
+
             except TypeError:
                 print('Target line is out of view')
                 # orient robot
-                ep_chassis.speed(x=0, y=0, z=np.rad2deg(np.pi/6))
+                ep_chassis.move(x=0, y=0, z=-15, z_speed=30).wait_for_completed()
 
-            plt.imshow(image)
-            plt.show()
+            # plt.imshow(image)
+            # plt.show()
+            cv2.imshow("img", image)
             cv2.waitKey(10)
 
         except KeyboardInterrupt:
             ep_camera.stop_video_stream()
             ep_robot.close()
-            printpyt('Exiting')
+            print('Exiting')
             exit(1)
