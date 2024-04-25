87557151fd5a7d50cc220dce793dbad2db5b8db2
gmann-13 <149419885+gmann-13@users.noreply.github.com>
2024-04-25 13:15:20 -0400

    added method for communicating that bot is at line

--- messagingserver.py
+++ messagingserver.py
@@ -1,10 +1,23 @@
 import time
 import zmq
 
-def StartPassingComms(ep_gripper=None):
+context = None
+socket = None
+waiting = None
+message = None
+
+def StartServer():
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
+
+
+def CommunicateAtLine(ep_gripper=None):
+    print("Sending request %s …" % request)
+    socket.send(b"Passer at Line")
+    print("Communicating that passer is at line")
+
+def StartPassingComms(ep_gripper=None):
     waiting = True
     while waiting:
 
