97c3a1ce4a3437588bab8909af1c2d1d56e9553f
Tejas Guha <10423046+roofTiles@users.noreply.github.com>
2024-04-26 15:47:15 -0400

    Update gripping.py

--- gripping.py
+++ gripping.py
@@ -15,9 +15,7 @@ ep_chassis = ep_robot.chassis
 
 def LookDown(ep_gripper=None, ep_arm=None, x=0, y=30, power=50):
     # Move forward xmm, drop down ymm
-    print('hello')
     ep_arm.move(x=x, y=-y).wait_for_completed()
-    print('goodbye')
 
 def LookUp(ep_gripper=None, ep_arm=None, x=0, y=30, power=50):
     # Move forward xmm, drop down ymm
@@ -27,7 +25,6 @@ def GrabLego(ep_gripper=None, ep_arm=None, x=62, y=60, power=50):
     # open gripper
     ep_gripper.open(power=power)
     time.sleep(1)
-    #ep_gripper.pause()
 
     # Move forward xmm, drop down ymm
     ep_arm.move(x=x, y=-y).wait_for_completed()
@@ -35,12 +32,9 @@ def GrabLego(ep_gripper=None, ep_arm=None, x=62, y=60, power=50):
     # close gripper
     ep_gripper.close(power=power)
     time.sleep(1)
-    #ep_gripper.pause()
 
     # Move backward xmm, move up ymm
-    print('stuck here')
     ep_arm.move(x=-x, y=y).wait_for_completed()
-    print('done')
 
 def DropLego(ep_gripper=None, ep_arm=None, x=60, y=100, power=50):
     # Move forward xmm, drop down ymm
@@ -49,7 +43,6 @@ def DropLego(ep_gripper=None, ep_arm=None, x=60, y=100, power=50):
     # close gripper
     ep_gripper.open(power=power)
     time.sleep(1)
-    #ep_gripper.pause()
 
     # Move backward xmm, move up ymm
     ep_arm.move(x=-x, y=y).wait_for_completed()
